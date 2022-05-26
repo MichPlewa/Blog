c90c13345476e846efb02a9bddd571548b5a8964
Michal Plewa <themipsial@gmail.com>
2022-05-26 21:06:36 +0200

    need to fix validate

--- src/components/common/TextInput/TextInput.js
+++ src/components/common/TextInput/TextInput.js
@@ -1,14 +1,17 @@
 import { Form } from 'react-bootstrap';
 
 const TextInput = (props) => {
+  console.log(props);
   return (
     <Form.Group className="mb-3" style={{ width: '40%' }} controlId={props.id}>
       <Form.Label>{props.children}</Form.Label>
       <Form.Control
+        {...props.reg}
         type={props.type}
         placeholder={props.placeholder}
         onChange={props.onChange}
         value={props.value}
+        {...props.form.title && <span>This is require</span>}
       />
     </Form.Group>
   );
--- src/components/features/PostForm/PostForm.js
+++ src/components/features/PostForm/PostForm.js
@@ -1,7 +1,5 @@
 import { useEffect, useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
-import TextArea from '../../common/TextArea/TextArea';
-import TextInput from '../../common/TextInput/TextInput';
 import { useDispatch, useSelector } from 'react-redux';
 import { addPost, editPost, selectPostById } from '../../../Redux/postsRedux';
 import { useNavigate } from 'react-router-dom';
@@ -9,13 +7,19 @@ import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import { dateToStr } from '../../utils/dateToStr';
+import { useForm } from 'react-hook-form';
 
 const PostForm = (props) => {
+  const {
+    register,
+    handleSubmit: validate,
+    formState: { errors },
+  } = useForm();
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = () => {
+    setContentError(!content);
+    setDateError(!date);
     const post = {
       title: title,
       shortDescription: description,
@@ -23,13 +27,10 @@ const PostForm = (props) => {
       publishedDate: date,
       author: author,
     };
-    console.log(dateToStr(post.publishedDate));
-    if (props.variant === 'edit') {
-      dispatch(editPost({ ...post, id: props.postId }));
-    } else {
+    if (content && date) {
       dispatch(addPost(post));
+      navigate('/');
     }
-    navigate('/');
   };
   const singlePost = useSelector((state) =>
     selectPostById(state, props.postId)
@@ -49,38 +50,64 @@ const PostForm = (props) => {
   const [date, setDate] = useState('');
   const [description, setDescription] = useState('');
   const [content, setContent] = useState('');
-
+  const [contentError, setContentError] = useState(false);
+  const [dateError, setDateError] = useState(false);
   return (
-    <Form onSubmit={(e) => handleSubmit(e)}>
-      <TextInput
-        type="text"
-        placeholder="Enter Title"
-        id="title"
-        onChange={(e) => setTitle(e.target.value)}
-        value={title}
-      >
-        Title
-      </TextInput>
-      <TextInput
-        type="text"
-        placeholder="Enter Author"
-        id="author"
-        onChange={(e) => setAuthor(e.target.value)}
-        value={author}
-      >
-        Author
-      </TextInput>
-      <DatePicker selected={date} onChange={(date) => setDate(date)} />
-      <TextArea
-        as="textarea"
-        placeholder="Enter description"
-        rows="3"
-        id="content"
-        onChange={(e) => setDescription(e.target.value)}
-        value={description}
-      >
-        Short description
-      </TextArea>
+    <Form onSubmit={validate(handleSubmit)}>
+      <Form.Group className="mb-3" style={{ width: '40%' }} controlId="title">
+        <Form.Label>Title</Form.Label>
+        <Form.Control
+          {...register('title', { required: true })}
+          type="text"
+          placeholder="Enter Title"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
+        {errors.title && (
+          <small className="d-block form-text text-danger mt-2">
+            This field is required This field is required and minimum 3 letters
+          </small>
+        )}
+      </Form.Group>
+      <Form.Group className="mb-3" style={{ width: '40%' }} controlId="author">
+        <Form.Label>Author</Form.Label>
+        <Form.Control
+          {...register('author', { required: true, maxLength: 3 })}
+          type="text"
+          placeholder="Enter author"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
+        {errors.author && (
+          <small className="d-block form-text text-danger mt-2">
+            This field is required and maximum 3 letters
+          </small>
+        )}
+      </Form.Group>
+      <Form.Group>
+        <DatePicker selected={date} onChange={(date) => setDate(date)} />
+        {dateError && (
+          <small className="d-block form-text text-danger mt-2">
+            Date can't be empty
+          </small>
+        )}
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="description">
+        <Form.Label>shortDescription</Form.Label>
+        <Form.Control
+          {...register('shortDescription', { required: true, minLength: 20 })}
+          as="textarea"
+          rows="3"
+          placeholder="Enter description"
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
+        />
+        {errors.shortDescription && (
+          <small className="d-block form-text text-danger mt-2">
+            This field is required and minimum 20 letters
+          </small>
+        )}
+      </Form.Group>
       <Form.Group>
         <Form.Label value={content}>Main content</Form.Label>
         <ReactQuill
@@ -89,6 +116,11 @@ const PostForm = (props) => {
           onChange={setContent}
           placeholder="Enter content"
         />
+        {contentError && (
+          <small className="d-block form-text text-danger mt-2">
+            Content can't be empty
+          </small>
+        )}
       </Form.Group>
       <Button type="submit">AddPost</Button>
     </Form>
